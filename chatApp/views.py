2f0fcb4cc2294aa80071a1b3609e730deb4e8ccb
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-12-24 04:36:49 +0530

    Refactored homepage to take username and chatroom as input

--- chatApp/views.py
+++ chatApp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def chatroom(request):
+    print(request.path)
+    return render(request, 'chatroom.html')
