3baa296d2fa0d71e771c522f0b1c7ca4372fce12
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-12-23 19:29:15 +0530

    Homepage template initialised

--- vaartalaapProject/asgi.py
+++ vaartalaapProject/asgi.py
@@ -3,15 +3,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from chatApp.routing import wsUrlPatterns
+#from chatApp.routing import wsUrlPatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vaartalaapProject.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        URLRouter(
-            wsUrlPatterns
-        )
-    ),
-})
+    "http": get_asgi_application()})
+    #websocket": AllowedHostsOriginValidator(
+       # URLRouter(
+      #      wsUrlPatterns
+     #   )
+    #)
+
