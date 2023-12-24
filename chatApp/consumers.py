ec053ec48af64e62ef6649e823507f66f8c304e9
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-12-24 16:15:05 +0530

    NLTKJ message tokeniser and POS tagging implemented

--- chatApp/consumers.py
+++ chatApp/consumers.py
@@ -1,6 +1,8 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from nltk import pos_tag, word_tokenize, download, help
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
@@ -31,17 +33,44 @@ class ChatConsumer(WebsocketConsumer):
         )              
     def receive(self, text_data):
         jsonMessage = json.loads(text_data)
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                "type":"chat_message",
-                "room_id":jsonMessage["room_id"],
-                "username":jsonMessage["username"],
-                "message":jsonMessage["message"],
-            }
-        )
+        if(jsonMessage["message"]["text"].startswith(":tokenise:")):
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    "type":"nltk_message",
+                    "room_id":jsonMessage["room_id"],
+                    "username":jsonMessage["username"],
+                    "message":jsonMessage["message"],
+                }
+            )
+        else:
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    "type":"chat_message",
+                    "room_id":jsonMessage["room_id"],
+                    "username":jsonMessage["username"],
+                    "message":jsonMessage["message"],
+                }
+            )
     def chat_message(self, event):
         self.send(text_data=json.dumps({
                 "room_id":event["room_id"],
                 "username":event["username"],
                 "message":event["message"]}))   
+    def nltk_message(self, event):
+        baseText = event["message"]["text"]
+        baseText = baseText.lstrip(":tokenise:")
+        tokenisedString =''
+        try:
+            tokenisedString = pos_tag(word_tokenize(baseText))
+        except:
+            import nltk
+            nltk.download('averaged_perceptron_tagger')
+            tokenisedString = pos_tag(word_tokenize(baseText))
+        finally:
+            event["message"]["text"] = str(tokenisedString)
+        self.send(text_data=json.dumps({
+                "room_id":event["room_id"],
+                "username":event["username"],
+                "message":event["message"]}))           
