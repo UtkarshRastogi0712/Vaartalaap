e77adb7a3ee477371b1011984f9aa2c7ee776acc
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-12-24 16:39:30 +0530

    Channel Layer Datastore migrated to redis for prod build

--- vaartalaapProject/settings.py
+++ vaartalaapProject/settings.py
@@ -73,10 +73,19 @@ TEMPLATES = [
 ASGI_APPLICATION = 'vaartalaapProject.asgi.application'
 
 CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+'''
+CHANNEL_LAYERS = {
     'default' : {
         'BACKEND' : "channels.layers.InMemoryChannelLayer"
     }
-}
+}'''
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
