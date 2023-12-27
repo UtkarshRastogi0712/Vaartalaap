6161e590d3b10c54f3fe49cd013f1b6bee00862a
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-12-28 02:14:47 +0530

    Refactored consumer to return object type for tokenised messages

--- chatApp/consumers.py
+++ chatApp/consumers.py
@@ -33,7 +33,6 @@ class ChatConsumer(WebsocketConsumer):
         )              
     def receive(self, text_data):
         jsonMessage = json.loads(text_data)
-        print(self.nickname, " recieved ", jsonMessage["message"]["text"])
         if(jsonMessage["message"]["text"].startswith(":tokenise:")):
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
@@ -60,11 +59,10 @@ class ChatConsumer(WebsocketConsumer):
                 "username":event["username"],
                 "message":event["message"]}))   
     def nltk_message(self, event):
-        print(self.nickname, " recieved ", event["message"]["text"], type(event["message"]["text"]))
         baseText = event["message"]["text"]
-        baseText = baseText.lstrip(":tokenise:")
-        tokenisedString =''
-        if(self.nickname == event["username"]):
+        if(type(event["message"]["text"]) != dict):
+            baseText = baseText.lstrip(":tokenise:")
+            tokenisedString =''
             try:
                 tokenisedString = pos_tag(word_tokenize(baseText))
             except:
@@ -75,7 +73,7 @@ class ChatConsumer(WebsocketConsumer):
                 jsonObj = {}
                 for i in tokenisedString:
                     jsonObj[i[0]] = i[1]
-                event["message"]["text"] = json.dumps(jsonObj)
+                event["message"]["text"] = jsonObj
         self.send(text_data=json.dumps({
                 "room_id":event["room_id"],
                 "username":event["username"],
